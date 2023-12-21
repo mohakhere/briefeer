5d74fa6cec700452559126bb0b6589a0e25a7e46
Mohak Sethi <107026265+mohakhere@users.noreply.github.com>
2023-12-21 23:28:57 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,3 +5,11 @@ app = Flask(__name__)
 @app.route("/")
 def node():
     return "Home"
+
+@app.route("/get-user/<user_id>")
+def get_user(user_id):
+    user_data = {
+        "user_id": user_id,
+        "name": "Kary",
+        "email": "example@exmpl.com"
+    }
