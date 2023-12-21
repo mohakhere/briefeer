e5defc182626dec1dbc7e914f7662bae1646e288
Mohak Sethi <107026265+mohakhere@users.noreply.github.com>
2023-12-21 23:29:10 +0530

    Update main.py

--- main.py
+++ main.py
@@ -13,3 +13,17 @@ def get_user(user_id):
         "name": "Kary",
         "email": "example@exmpl.com"
     }
+    new = request.args.get("new_one")
+    if new:
+        user_data["new_one"] = new
+    
+    return jsonify(user_data), 200
+
+@app.route("/create_user", methods=["POST"])
+def create_user():
+    data = request.get_json()
+    #return jsonify(data), 201
+    return ("User " + str(data["user_name"]) + " Created Successfully"), 201 
+
+if __name__== "__main__":
+    app.run(debug=True)
