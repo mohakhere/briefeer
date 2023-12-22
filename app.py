189595f1ccc0ea5d09e2fcc2fd62be619ca3f2ef
Mohak Sethi <107026265+mohakhere@users.noreply.github.com>
2023-12-22 22:31:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,3 +7,18 @@ app = Flask(__name__)
 videos = UploadSet('videos', extensions=('mp4', 'avi', 'mkv'))
 app.config['UPLOADED_VIDEOS_DEST'] = 'uploads'
 configure_uploads(app, videos)
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/upload', methods=['POST'])
+def upload():
+    if 'video' in request.files:
+        video = request.files['video']
+        video_path = f"uploads/{video.filename}"
+        video.save(video_path)
+        return redirect(url_for('index'))
+    return 'Error uploading video'
+
+if __name__ == '__main__':
+    app.run(debug=True)
